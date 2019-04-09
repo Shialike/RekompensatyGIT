4266bce2c2654f3e7b89cc6441c0b2fc80363f30
Shialike <shialike@gmail.com>
2019-04-09 21:14:04 +0200

    Packages update

--- Rekompensaty.DataAccess/dbModel.Designer.cs
+++ Rekompensaty.DataAccess/dbModel.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\shial\Documents\Visual Studio 2015\Projects\RekompensatyGIT\Rekompensaty.DataAccess\dbModel.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\SRC\RekompensatyGIT\Rekompensaty.DataAccess\dbModel.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
